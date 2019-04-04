1b407e980316aae4a62a4810b0baa05bf1b77813
Marko Cupic <m.cupic@gmx.ch>
2019-04-04 20:10:23 +0200

    dev

--- assets/js/ivm_immo_collection.js
+++ assets/js/ivm_immo_collection.js
@@ -1,7 +1,7 @@
 /*
  * This file is part of Contao Ivm Immo Collection.
  *
- * (c) Marko Cupic
+ * (c) Marko Cupic, april 2019
  * @author Marko Cupic <https://github.com/markocupic/ivm_immo_collection>
  * @license MIT
  *
